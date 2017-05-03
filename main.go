20fcc32f9276c6df4a584b0ecad8e9ee587157d2
zricethezav <zricezrice@gmail.com>
2017-05-02 22:21:40 -0500

    progress

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"golang.org/x/net/html"
+	"time"
+)
+
+// constants
+const (
+	twitterTimeStringFmt = "15:04 PM - 2 Jan 2006"
+	localTimeZone = "CST" // timezone where the compute is happening
 )
 
 type TweetHeader struct {
@@ -27,6 +34,11 @@ func main() {
 	// TODO:
 	// 	- check if private account of not
 	// 	- continuous loading (lookup api req to load more)
+	// 	- set timezone
+	// 	- think about data stores?
+	// 	- finish tweetfooter retriever, will probably need to follow actual tweet link to get
+	//	  all the likes, retweets, and replies available for public (big ticket here)
+
 	resp, err := http.Get("https://twitter.com/realDonaldTrump")
 	if err != nil {
 		panic(err)
@@ -39,20 +51,26 @@ func main() {
 	tweets := []Tweet{}
 	var currTweet Tweet
 	currEleTweet := false
+
 	// tree traversal
-	var f func(*html.Node)
-	f = func(n *html.Node) {
+	var htmlTweetParser func(*html.Node)
+	htmlTweetParser = func(n *html.Node) {
+		// check if node type is div, if yes investigate attributes of the node
 		if n.Type == html.ElementNode && n.Data == "div" {
 			for _, a := range n.Attr {
 				if a.Key == "class" {
 					if a.Val == "stream-item-header" {
-						currTweet.tweetHeader = processTweetHeader(n)
+						currTweet.tweetHeader = retrieveTweetHeader(n)
 						currEleTweet = true
 					} else if a.Val == "js-tweet-text-container" {
-						currTweet.tweetText = processTweet(n)
+						currTweet.tweetText = retrieveTweet(n)
+					} else if a.Val == "stream-item-footer" {
+						currTweet.tweetFooter = retrieveTweetFooter(n)
 					}
 				}
 			}
+			// if current node contains attributes that signal a tweet, add to
+			// tweet slice and reset currEleTweet flag
 			if currEleTweet{
 				currEleTweet = false
 				tweets = append(tweets, currTweet)
@@ -60,17 +78,18 @@ func main() {
 		}
 
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
+			htmlTweetParser(c)
 		}
 	}
-	f(root)
+	htmlTweetParser(root)
 	fmt.Println(tweets)
 }
 
-func processTweet(n *html.Node) string {
+// retrieve 
+func retrieveTweet(n *html.Node) string {
 	tweet := ""
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		tweet += processTweet(c)
+		tweet += retrieveTweet(c)
 	}
 	if n.Type == html.TextNode {
 		return string(n.Data)
@@ -78,7 +97,7 @@ func processTweet(n *html.Node) string {
 	return tweet
 }
 
-func processTweetHeader(n *html.Node) TweetHeader {
+func retrieveTweetHeader(n *html.Node) TweetHeader {
 	var header TweetHeader
 
 	var processTweetHeaderHelper func(n *html.Node)
@@ -86,12 +105,10 @@ func processTweetHeader(n *html.Node) TweetHeader {
 		for e := n.FirstChild; e != nil; e = e.NextSibling {
 			for _, a1 := range e.Attr {
 				if a1.Key == "title" {
-					fmt.Println("time: ", a1.Val)
 					header.stringTime = a1.Val
 					header.time = stringTimeToUnixTime(header.stringTime)
 				}
 				if a1.Key == "href" {
-					fmt.Println("link: ", a1.Val)
 					header.tweetUrl = a1.Val
 				}
 			}
@@ -109,8 +126,7 @@ func processTweetHeader(n *html.Node) TweetHeader {
 	return header
 }
 
-// TODO this guy
-func processTweetFooter(n *html.Node) TweetFooter {
+func retrieveTweetFooter(n *html.Node) TweetFooter {
 	var footer TweetFooter
 	var processTweetFooterHelper func(n *html.Node)
 	processTweetFooterHelper = func(n *html.Node) {
@@ -137,11 +153,14 @@ func processTweetFooter(n *html.Node) TweetFooter {
 	return footer
 }
 
+// perhaps split time operations into its own util file
+// https://golang.org/src/time/format.go <--- for reference
 func stringTimeToUnixTime(stringTime string) int64 {
-	return 1
+	tweetTime, err := time.Parse(twitterTimeStringFmt, stringTime)
+	if err != nil {
+		return -1
+	}
+	return tweetTime.Unix()
 }
 
 
-func insertTweet() {
-
-}
